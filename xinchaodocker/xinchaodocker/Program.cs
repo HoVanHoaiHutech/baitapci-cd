a0cd8f7c734b185a10dc42b8369a1b9993f92452
hồ văn hoài <96343457+HoVanHoaiHutech@users.noreply.github.com>
2026-03-27 10:41:09 +0700

    Update Program.cs

--- xinchaodocker/xinchaodocker/Program.cs
+++ xinchaodocker/xinchaodocker/Program.cs
@@ -5,6 +5,8 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+app.MapGet("/env", (IHostEnvironment env) => $"Environment: {env.EnvironmentName}");
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
